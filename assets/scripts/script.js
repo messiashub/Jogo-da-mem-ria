055ff28bcdc8b2e1a9119783dad124a0c0630887
messiashub <claudineimessias@hotmail.com>
2020-11-16 17:25:45 -0300

    Embaralhando as cartas

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -14,6 +14,29 @@ let techs = [
     'node',
     'react'
 ];
+let cards = null;
+startGame();
+
+function startGame(){    // FUNÇÃO PARA COMEÇAR O JOGO
+    cards = creatCardsFromTechs(techs);
+    embaralharCards(cards);
+    console.log(cards);
+}
+
+function embaralharCards(techs){ //FUNÇÃO PÁRA EMBARALHAR AS CARTAS
+    let indiceAtual = cards.length;
+    let randomIndice = 0;
+
+    while(indiceAtual !== 0){
+
+        randomIndice = Math.floor(Math.random() * indiceAtual);
+        indiceAtual--;
+
+        [cards[randomIndice], cards[indiceAtual]] =  [cards[indiceAtual], cards[randomIndice]];
+    }
+
+}
+
 creatCardsFromTechs(techs);
 
 function creatCardsFromTechs(techs) {  // FUNÇÃO PARA CRIAR CARTAS
