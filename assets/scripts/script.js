171edf3640ace920769cee5aafbc4dd2464bd3e9
messiashub <claudineimessias@hotmail.com>
2020-11-16 23:15:12 -0300

    Montando o tabuleiro

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,8 @@
 
 const FRONT = "card_front";
-const BACK = "card-back";
+const BACK = "card_back";
+const CARD = 'card';
+const ICON = "icon";
 
 let techs = [
     'bootstrap',
@@ -20,10 +22,52 @@ startGame();
 function startGame(){    // FUNÇÃO PARA COMEÇAR O JOGO
     cards = creatCardsFromTechs(techs);
     embaralharCards(cards);
+    inicializarCards(cards);
     console.log(cards);
 }
 
-function embaralharCards(techs){ //FUNÇÃO PÁRA EMBARALHAR AS CARTAS
+function inicializarCards(cards){ // FUNÇÃO PARA PEGAR OS MODELOS DAS CARTAS E TRANSFORMAR EM  VISUAL
+    let gameBoard = document.getElementById('gameBoard');
+
+    cards.forEach(card =>{
+        let cardElement = document.createElement('div');
+        cardElement.id = card.id;
+        cardElement.classList.add(CARD);
+        cardElement.dataset.icon = card.icon;
+
+        createCardContent(card, cardElement);
+
+        cardElement.addEventListener('click', flipCard);
+        gameBoard.appendChild(cardElement);
+    })
+    
+}
+
+function createCardContent(card, cardElement){   
+
+    creatCardFace(FRONT, card, cardElement);
+    creatCardFace(BACK, card, cardElement);
+
+}
+
+function creatCardFace(face, card, element){    // FUNÇÃO PARA CRIAR ELEMENTOS HTML
+
+    let cardElementFace = document.createElement('div');
+    cardElementFace.classList.add(face);
+    if(face===FRONT){
+        let iconElement = document.createElement('img');
+        iconElement.classList.add(ICON);
+        iconElement.src = "./assets/imagens/" + card.icon + ".png";
+        cardElementFace.appendChild(iconElement);
+    }else{
+        cardElementFace.innerHTML = "&lt/&gt";
+    }
+    element.appendChild(cardElementFace);
+}
+
+
+
+function embaralharCards(techs){ //FUNÇÃO PARA EMBARALHAR AS CARTAS
     let indiceAtual = cards.length;
     let randomIndice = 0;
 
@@ -43,10 +87,10 @@ function creatCardsFromTechs(techs) {  // FUNÇÃO PARA CRIAR CARTAS
 
     let cards = [];
 
-    for (let tech of techs) {
+    techs.forEach((tech) => {
         cards.push(createPairFromTech(tech));
-    }
-
+    }) 
+        
     return cards.flatMap(pair => pair);
 }
 
@@ -65,5 +109,10 @@ function createPairFromTech(tech) {  // FUNÇÃO PARA CRIAR  PARES DE CARTAS
 
 function createIdWithTech(tech) {  // FUNÇÃO PARA CRIAR ID RANDÔMICO
     return tech + parseInt(Math.random() * 1000);
+
+}
+
+function flipCard(){
+    this.classList.add("flip");
 
 }
