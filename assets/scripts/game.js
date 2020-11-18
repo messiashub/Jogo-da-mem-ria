3022c8c7e70b557f55f45350cd383e31d5c801a3
messiashub <claudineimessias@hotmail.com>
2020-11-17 23:42:22 -0300

    Achando e corrigindo bugs

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,37 +1,48 @@
-let game ={
+let game = {
 
     lockMode: false,
     firstCard: null,
     secondCard: null,
 
-    setCard: function(id){
-        let card = this.cards.filter(card=>card.id ===id)[0];
+    setCard: function (id) {
+        let card = this.cards.filter(card => card.id === id)[0];
+        console.log(card);
 
-        if(card.flipped || this.lockMode){
+        if (card.flipped || this.lockMode) {
             return false;
         }
 
-        if(!this.firstCard){
+        if (!this.firstCard) {
             this.firstCard = card;
+            this.firstCard.flipped = true;
             return true;
-        }else{
+        } else {
             this.secondCard = card;
+            this.secondCard.flipped = true;
             this.lockMode = true;
             return true;
         }
 
     },
 
-    checkMatch: function(){
+    checkMatch: function () {
+        if (!this.firstCard || !this.secondCard) {
+            return false;
+        }
         return this.firstCard.icon === this.secondCard.icon;
     },
-     clearCards: function(){
-         this.firstCard = null;
-         this.secondCard = null;
-         this.lockMode = false;
+    clearCards: function () {
+        this.firstCard = null;
+        this.secondCard = null;
+        this.lockMode = false;
+    },
+    unflipCards(){
+        this.firstCard.flipped = false;
+        this.secondCard.flipped = false;
+        this.clearCards();
     },
 
-    techs : [
+    techs: [
         'bootstrap',
         'css',
         'electron',
@@ -43,10 +54,10 @@ let game ={
         'node',
         'react'
     ],
-    cards : null,
+    cards: null,
 
-    
-   /*  createCardsFromTechs(techs);  */
+
+    /*  createCardsFromTechs(techs);  */
 
     createCardsFromTechs: function () {  // FUNÇÃO PARA CRIAR CARTAS
 
@@ -54,8 +65,8 @@ let game ={
 
         this.techs.forEach((tech) => {
             this.cards.push(this.createPairFromTech(tech));
-        }) 
-            
+        })
+
         this.cards = this.cards.flatMap(pair => pair);
         this.embaralharCards();
         return this.cards;
@@ -74,21 +85,21 @@ let game ={
         }]
     },
 
-    createIdWithTech:function(tech) {  // FUNÇÃO PARA CRIAR ID RANDÔMICO
+    createIdWithTech: function (tech) {  // FUNÇÃO PARA CRIAR ID RANDÔMICO
         return tech + parseInt(Math.random() * 1000);
 
     },
-    embaralharCards: function(cards){ //FUNÇÃO PARA EMBARALHAR AS CARTAS
+    embaralharCards: function (cards) { //FUNÇÃO PARA EMBARALHAR AS CARTAS
         let indiceAtual = this.cards.length;
         let randomIndice = 0;
-    
-        while(indiceAtual !== 0){
-    
+
+        while (indiceAtual !== 0) {
+
             randomIndice = Math.floor(Math.random() * indiceAtual);
             indiceAtual--;
-    
-            [this.cards[randomIndice], this.cards[indiceAtual]] =  [this.cards[indiceAtual], this.cards[randomIndice]];
+
+            [this.cards[randomIndice], this.cards[indiceAtual]] = [this.cards[indiceAtual], this.cards[randomIndice]];
         }
-    
+
     }
 }
